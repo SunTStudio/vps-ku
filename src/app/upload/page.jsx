97c7f3748b5b07230c754f7a0fa23c395750d5f7
SunTStudio <mahsunmuh0@gmail.com>
2026-06-04 16:09:53 +0700

    c

--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,11 +1,11 @@
 
-// function page() {
-//   return (
-//     <div className="main-heading">
-//         <h1>About Us</h1>
-//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem excepturi numquam, maxime repudiandae ut sint dolorum mollitia necessitatibus fugit esse iure, ducimus nostrum recusandae earum. Aliquid molestias quam culpa? Minus magnam possimus commodi, suscipit dolorum consectetur numquam explicabo fugiat, accusantium molestiae officia, harum odio hic et praesentium. Ex sequi perspiciatis sit corrupti est recusandae nulla, tenetur doloribus mollitia necessitatibus aspernatur ullam illum iure accusamus dicta, voluptates eum debitis reprehenderit laboriosam maxime totam quos. Amet sapiente repudiandae, excepturi labore reiciendis placeat numquam voluptatem tempore suscipit in quos accusamus accusantium, iusto architecto a? Maiores exercitationem corporis dolore voluptas ipsum ea reiciendis. Fuga voluptatibus quas eius autem fugit enim deserunt ducimus debitis culpa asperiores. Quisquam rerum officiis explicabo deleniti mollitia quam eum aliquid laudantium facere molestiae ea at quae fuga quibusdam cum perferendis magni, vitae blanditiis laborum iste facilis error ipsum. Officiis nesciunt fugiat soluta repudiandae error laboriosam alias culpa animi, a nostrum quibusdam quae, rem mollitia qui. Quae incidunt at architecto laudantium, ullam ab exercitationem laboriosam accusantium ducimus esse nobis soluta vel dignissimos impedit asperiores voluptatem quasi commodi non. Neque aliquam aliquid esse enim quaerat deleniti voluptas ipsa molestias quae, et voluptatibus minima beatae? Eaque beatae illum asperiores perspiciatis maxime, pariatur nostrum?</p>
-//     </div>
-//   )
-// }
+function page() {
+  return (
+    <div className="main-heading">
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem excepturi numquam, maxime repudiandae ut sint dolorum mollitia necessitatibus fugit esse iure, ducimus nostrum recusandae earum. Aliquid molestias quam culpa? Minus magnam possimus commodi, suscipit dolorum consectetur numquam explicabo fugiat, accusantium molestiae officia, harum odio hic et praesentium. Ex sequi perspiciatis sit corrupti est recusandae nulla, tenetur doloribus mollitia necessitatibus aspernatur ullam illum iure accusamus dicta, voluptates eum debitis reprehenderit laboriosam maxime totam quos. Amet sapiente repudiandae, excepturi labore reiciendis placeat numquam voluptatem tempore suscipit in quos accusamus accusantium, iusto architecto a? Maiores exercitationem corporis dolore voluptas ipsum ea reiciendis. Fuga voluptatibus quas eius autem fugit enim deserunt ducimus debitis culpa asperiores. Quisquam rerum officiis explicabo deleniti mollitia quam eum aliquid laudantium facere molestiae ea at quae fuga quibusdam cum perferendis magni, vitae blanditiis laborum iste facilis error ipsum. Officiis nesciunt fugiat soluta repudiandae error laboriosam alias culpa animi, a nostrum quibusdam quae, rem mollitia qui. Quae incidunt at architecto laudantium, ullam ab exercitationem laboriosam accusantium ducimus esse nobis soluta vel dignissimos impedit asperiores voluptatem quasi commodi non. Neque aliquam aliquid esse enim quaerat deleniti voluptas ipsa molestias quae, et voluptatibus minima beatae? Eaque beatae illum asperiores perspiciatis maxime, pariatur nostrum?</p>
+    </div>
+  )
+}
 
-// export default page
+export default page
