1d9bde56d8df34eb3e0bfea30179a19b81d4bbef
SunTStudio <mahsunmuh0@gmail.com>
2026-06-04 16:12:21 +0700

    c

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="header">
           <nav>
-            <div className="logo"><Link href="/"><Image src="vercel.svg" width="30" height="30"></Image></Link></div>
+            {/* <div className="logo"><Link href="/"><Image src="vercel.svg" width="30" height="30"></Image></Link></div> */}
             <div className="nav-links">
             <Link href="/">Home</Link>
             <Link href="/upload">About</Link>
